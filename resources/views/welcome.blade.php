a1fc287d3340197b86df3a0ff6d573eeceec5007
byNikx <w3c.nadeem@hotmail.com>
2017-02-15 19:12:50 +0530

    15-02-2017 17:15 Update

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,95 +1,150 @@
 <!DOCTYPE html>
 <html lang="{{ config('app.locale') }}">
-    <head>
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel</title>
+    <style>
+    body {
+    background-color: #e8e8e8;
+    }
 
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
+    *[ng-cloak] {
+    display: none;
+    }
 
-        <!-- Styles -->
-        <style>
-            html, body {
-                background-color: #fff;
-                color: #636b6f;
-                font-family: 'Raleway', sans-serif;
-                font-weight: 100;
-                height: 100vh;
-                margin: 0;
+    [gutter-default-top] {
+    margin-top: 2em;
+    }
+
+    .nx-paper {
+    background-color: #fff;
+    display: block;
+    }
+
+    .nx-container {
+    display: block;
+    }
+    .nx-container.xs {
+    margin-top: 0;
+    margin-right: 0;
+    margin-bottom: 0;
+    margin-left: 0;
+    }
+    .nx-container.gt-xs {
+    margin-top: 0;
+    margin-right: 5%;
+    margin-bottom: 0;
+    margin-left: 5%;
+    }
+    .nx-container.gt-sm {
+    margin-top: 0;
+    margin-right: 7.5%;
+    margin-bottom: 0;
+    margin-left: 7.5%;
+    }
+    .nx-container.gt-md {
+    margin-top: 0;
+    margin-right: 10%;
+    margin-bottom: 0;
+    margin-left: 10%;
+    }
+    .nx-container.gt-lg {
+    margin-top: 0;
+    margin-right: 15%;
+    margin-bottom: 0;
+    margin-left: 15%;
+    }
+    </style>
+
+    <title>Angular Laravel - App</title>
+    <link rel="stylesheet" href="http://ajax.googleapis.com/ajax/libs/angular_material/1.1.0/angular-material.min.css">
+  </head>
+  <body ng-app="NXBlog" ng-cloak>
+    <md-toolbar></md-toolbar>
+    <nx-container>
+      <md-card>  
+          ';kl';l
+      </md-card>
+    </nx-container>
+    <!-- Angular Material requires Angular.js Libraries -->
+    <script src="http://ajax.googleapis.com/ajax/libs/angularjs/1.5.5/angular.min.js"></script>
+    <script src="http://ajax.googleapis.com/ajax/libs/angularjs/1.5.5/angular-animate.min.js"></script>
+    <script src="http://ajax.googleapis.com/ajax/libs/angularjs/1.5.5/angular-aria.min.js"></script>
+    <script src="http://ajax.googleapis.com/ajax/libs/angularjs/1.5.5/angular-messages.min.js"></script>
+
+    <!-- Angular Material Library -->
+    <script src="http://ajax.googleapis.com/ajax/libs/angular_material/1.1.0/angular-material.min.js"></script>
+
+    <!-- Your application bootstrap  -->
+    <script type="text/javascript">
+      /**
+      * You must include the dependency on 'ngMaterial'
+      */
+      var app = angular.module('NXBlog', ['ngMaterial']);
+
+      app.impl = app.impl || {};
+      app.impl.nxContainer = ['$mdMedia', function($mdMedia){
+
+        let linkImpl = function(scope, element, attr){
+          scope.$mdMedia = $mdMedia;
+          if(!element.hasClass('nx-container'))
+            element.addClass('nx-container');
+
+          let breakpoint = {
+                gtLarge : 'gt-lg',
+                gtMedium : 'gt-md',
+                gtSmall : 'gt-sm',
+                gtXtraSmall : 'gt-xs',
+                xtraSmall : 'xs'
+          };
+
+          let containerDecorator = function(newValue, oldValue){
+            element.removeClass(
+              Object.values(breakpoint).join(' ')
+            );
+            if($mdMedia(breakpoint.gtLarge)){
+               element.addClass(breakpoint.gtLarge);
+            }else if($mdMedia(breakpoint.gtMedium)){
+               element.addClass(breakpoint.gtMedium);
+            }else if($mdMedia(breakpoint.gtSmall)){
+               element.addClass(breakpoint.gtSmall);
+            }else if($mdMedia(breakpoint.gtXtraSmall)){
+               element.addClass(breakpoint.gtXtraSmall);
+            }else{
+              element.addClass(breakpoint.xtraSmall);
             }
+          };
+          scope.$watchCollection(function(){
+            return [
+              $mdMedia('gt-xs'),
+              $mdMedia('gt-sm'),
+              $mdMedia('gt-md'),
+              $mdMedia('gt-lg')
+            ];
+          }, containerDecorator);
+        }
+        return {
+          restrict : 'EAC',
+          link : linkImpl
+        };
+      }];
 
-            .full-height {
-                height: 100vh;
-            }
 
-            .flex-center {
-                align-items: center;
-                display: flex;
-                justify-content: center;
-            }
+      app.impl.nxPaper = [function(){
+        let linkImpl = function(scope, element, attr){
+          element.addClass('nx-paper md-whiteframe-1dp');
+        }
+        return {
+          restrict : 'AEC',
+          link : linkImpl
+        }
+      }];
 
-            .position-ref {
-                position: relative;
-            }
 
-            .top-right {
-                position: absolute;
-                right: 10px;
-                top: 18px;
-            }
 
-            .content {
-                text-align: center;
-            }
-
-            .title {
-                font-size: 84px;
-            }
-
-            .links > a {
-                color: #636b6f;
-                padding: 0 25px;
-                font-size: 12px;
-                font-weight: 600;
-                letter-spacing: .1rem;
-                text-decoration: none;
-                text-transform: uppercase;
-            }
-
-            .m-b-md {
-                margin-bottom: 30px;
-            }
-        </style>
-    </head>
-    <body>
-        <div class="flex-center position-ref full-height">
-            @if (Route::has('login'))
-                <div class="top-right links">
-                    @if (Auth::check())
-                        <a href="{{ url('/home') }}">Home</a>
-                    @else
-                        <a href="{{ url('/login') }}">Login</a>
-                        <a href="{{ url('/register') }}">Register</a>
-                    @endif
-                </div>
-            @endif
-
-            <div class="content">
-                <div class="title m-b-md">
-                    Laravel
-                </div>
-
-                <div class="links">
-                    <a href="https://laravel.com/docs">Documentation</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
-                </div>
-            </div>
-        </div>
-    </body>
+      app.directive('nxContainer', app.impl.nxContainer);
+    </script>
+  </body>
 </html>
