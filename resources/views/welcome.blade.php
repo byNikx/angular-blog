940fb560f2cb15bb64c25807f4de9cff849fce34
byNikx <w3c.nadeem@hotmail.com>
2017-02-20 19:20:57 +0530

    20-02-2017 19:20

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -58,17 +58,47 @@
     margin-left: 15%;
     }
     .post-wrapper{
-      margin-bottom: 1.4em;
+      margin-bottom: 2em;
     }
-    .post-wrapper .post-title{
+    .post-wrapper .post-title, .post-owner{
       text-decoration: none;
-      color: rgba(0, 0, 0, 0.84);
+      color: rgba(0, 0, 0, 0.87);
     }
 
-
+    .no-margin{
+      margin: 0;
+    }
     .label-type-1{
       margin-bottom: 10px;
     }
+    </style>
+
+    <style>
+     .tag-list{
+       margin:0;
+       padding:0;
+       list-style:none;
+     }
+     .tag-list li{
+       display: inline-block;
+       margin: 0 5px;
+     }
+     .tag-list li a{
+       background: rgb(224,224,224);
+       color: rgb(66,66,66);
+       padding: 3px 10px;
+       font-size: 14px;
+       font-weight: 400;
+       border-radius: 3px;
+       position: relative;
+       text-decoration: none;
+     }
+     .tag-list li:first-child{
+       margin-left: 0;
+     }
+     .tag-list li:last-child{
+       margin-right: 0;
+     }
     </style>
 
     <title>Angular Laravel - App</title>
@@ -78,12 +108,28 @@
   </head>
   <body ng-app="NXBlog" ng-cloak layout="column">
     <md-toolbar md-whiteframe>
+      <section layout md-colors="{'background-color':'primary-hue-2'}" style="font-size:14px; font-weight:400;display:none">
+        <nx-container layout-padding>
+          &nbsp
+        </nx-container>
+      </section>
       <nx-container>
       <div class="md-toolbar-tools">
           <md-button class="md-icon-button">
             <md-icon>menu</md-icon>
           </md-button>
-          <h2>Nikx</h2>
+          <h2>Tech</h2>
+          <div flex layout-margin hide-xs>
+            <md-autocomplete md-no-cache="true" md-selected-item="selectedItem" md-items="item in items" placeholder="looking for someting?" md-search-text="searchText" md-item-text="item.display">
+              <span>{[{ item.display }]}</span>
+            </md-autocomplete>
+          </div>
+          <div hide-xs layout layout-align="center center">
+            <md-button class="md-icon-button">
+              <md-icon>notifications</md-icon>
+            </md-button>
+            <div class="nav-avatar" md-colors="{'background-color':'accent'}" style="width:30px; height:30px; border-radius:30px; font-size:15px;" layout="row" layout-align="center center">N</div>
+          </div>
       </div>
     </nx-container>
     </md-toolbar>
@@ -92,48 +138,172 @@
       <nx-container layout="row" layout-margin>
         <section flex>
             <h3 class="md-headline label-type-1" layout-margin>Yesterday</h3>
-            <md-card>
-        <md-card-header>
-          <md-card-avatar>
-            <img src="https://material.angularjs.org/latest/img/logo.svg"/>
-          </md-card-avatar>
-          <md-card-header-text>
-            <span class="md-title">Angular</span>
-            <span class="md-subhead">Material</span>
-          </md-card-header-text>
-        </md-card-header>
-        <img ng-src="https://awcdn1.ahmad.works/writing/wp-content/uploads/2015/05/Resturant-Employer-940x400.jpg" class="md-card-image" alt="Washed Out">
-        <md-card-title>
-         <md-card-title-text>
-           <span class="md-headline">In-card mixed actions</span>
-           <ul style="margin:0;padding:0; list-style:none;">
-             <li ng-repeat="i in [1,2,3,4]" style="display:inline-block;">{[{i}]}</li>
-           </ul>
-         </md-card-title-text>
-       </md-card-title>
-        <md-card-content>
-          <p>
-            The titles of Washed Out's breakthrough song and the first single from Paracosm share the
-            two most important words in Ernest Greene's musical language: feel it. It's a simple request, as well...
-          </p>
-        </md-card-content>
-        <md-card-actions layout="row" layout-align="start center" layout-padding>
-          <div style="padding:0;">
-            <md-button class="md-raised md-primary">Continue Reading</md-button>
-          </div>
-          <md-card-icon-actions hide>
-            <md-button class="md-icon-button" aria-label="toggle">
-              <md-icon>arrow_drop_down</md-icon>
-            </md-button>
-          </md-card-icon-actions>
-        </md-card-actions>
-      </md-card>
+            <md-card ng-href="#/post/287897" class="post-wrapper">
+              <md-card-header>
+                <md-card-avatar>
+                  <img src="https://avatars0.githubusercontent.com/u/16052650?v=3&s=460"/>
+                </md-card-avatar>
+                <md-card-header-text>
+                  <span>
+                    <a ng-href="#/nikx-fabrizio/" class="post-owner">
+                      <span class="md-title">Nikx Fabrizio</span>
+                    </a>
+                  </span>
+                  <span class="md-subhead">2 days ago</span>
+                </md-card-header-text>
+              </md-card-header>
+              <img ng-src="https://awcdn1.ahmad.works/writing/wp-content/uploads/2015/05/Resturant-Employer-940x400.jpg" class="md-card-image" alt="Washed Out">
+              <md-card-title>
+               <md-card-title-text>
+                 <div>
+                   <a ng-href="#!/google" class="post-title">
+                     <h2 class="md-headline no-margin">In-card mixed actions</h2>
+                   </a>
+                 </div>
+               </md-card-title-text>
+             </md-card-title>
+              <md-card-content>
+                <p>
+                  The titles of Washed Out's breakthrough song and the first single from Paracosm share the
+                  two most important words in Ernest Greene's musical language: feel it. It's a simple request, as well...
+                </p>
+                <ul class="tag-list">
+                  <li ng-repeat="tag in ['angularjs', 'javascript', 'HTML5']">
+                    <a ng-href="#!/tags/{[{tag}]}" md-ink-ripple class="md-secondary">{[{tag}]}</a>
+                  </li>
+                </ul>
+              </md-card-content>
+              <md-card-actions layout="row" layout-align="start center" layout-padding style="display:none">
+                <div style="padding:0; display:none">
+                  <md-button class="md-raised md-primary">Continue Reading</md-button>
+                </div>
+              </md-card-actions>
+              <md-card-icon-actions layout layout-padding>
+                <span flex></span>
+                <div>
+                  <md-icon>question_answer</md-icon>
+                  <span>1.3k</span>
+                </div>
+                <div>
+                  <md-icon>favorite</md-icon>
+                  <span>2.4k</span>
+                </div>
+              </md-card-icon-actions>
+            </md-card>
+
+            <md-card ng-href="#/post/287897" class="post-wrapper">
+              <md-card-header>
+                <md-card-avatar>
+                  <img src="https://avatars0.githubusercontent.com/u/16052650?v=3&s=460"/>
+                </md-card-avatar>
+                <md-card-header-text>
+                  <span>
+                    <a ng-href="#/nikx-fabrizio/" class="post-owner">
+                      <span class="md-title">Nikx Fabrizio</span>
+                    </a>
+                  </span>
+                  <span class="md-subhead">2 days ago</span>
+                </md-card-header-text>
+              </md-card-header>
+              <md-card-title>
+               <md-card-title-text>
+                 <div>
+                   <a ng-href="#!/google" class="post-title">
+                     <h2 class="md-headline no-margin">In-card mixed actions</h2>
+                   </a>
+                 </div>
+               </md-card-title-text>
+             </md-card-title>
+              <md-card-content>
+                <p>
+                  The titles of Washed Out's breakthrough song and the first single from Paracosm share the
+                  two most important words in Ernest Greene's musical language: feel it. It's a simple request, as well...
+                </p>
+                <ul class="tag-list">
+                  <li ng-repeat="tag in ['angularjs', 'javascript', 'HTML5']">
+                    <a ng-href="#!/tags/{[{tag}]}" md-ink-ripple class="md-secondary">{[{tag}]}</a>
+                  </li>
+                </ul>
+              </md-card-content>
+              <md-card-actions layout="row" layout-align="start center" layout-padding style="display:none">
+                <div style="padding:0; display:none">
+                  <md-button class="md-raised md-primary">Continue Reading</md-button>
+                </div>
+              </md-card-actions>
+              <md-card-icon-actions layout layout-padding>
+                <span flex></span>
+                <div>
+                  <md-icon>question_answer</md-icon>
+                  <span>1.3k</span>
+                </div>
+                <div>
+                  <md-icon>favorite</md-icon>
+                  <span>2.4k</span>
+                </div>
+              </md-card-icon-actions>
+            </md-card>
+
+
+            <md-card ng-href="#/post/287897" class="post-wrapper">
+              <md-card-header>
+                <md-card-avatar>
+                  <img src="https://avatars0.githubusercontent.com/u/16052650?v=3&s=460"/>
+                </md-card-avatar>
+                <md-card-header-text>
+                  <span>
+                    <a ng-href="#/nikx-fabrizio/" class="post-owner">
+                      <span class="md-title">Nikx Fabrizio</span>
+                    </a>
+                  </span>
+                  <span class="md-subhead">2 days ago</span>
+                </md-card-header-text>
+              </md-card-header>
+              <iframe height="349" src="http://www.youtube.com/embed/9CcpAMjpA48?rel=0&hd=1" frameborder="0" allowfullscreen></iframe>
+              <md-card-title>
+               <md-card-title-text>
+                 <div>
+                   <a ng-href="#!/google" class="post-title">
+                     <h2 class="md-headline no-margin">In-card mixed actions</h2>
+                   </a>
+                 </div>
+               </md-card-title-text>
+             </md-card-title>
+              <md-card-content>
+                <p>
+                  The titles of Washed Out's breakthrough song and the first single from Paracosm share the
+                  two most important words in Ernest Greene's musical language: feel it. It's a simple request, as well...
+                </p>
+                <ul class="tag-list">
+                  <li ng-repeat="tag in ['angularjs', 'javascript', 'HTML5']">
+                    <a ng-href="#!/tags/{[{tag}]}" md-ink-ripple class="md-secondary">{[{tag}]}</a>
+                  </li>
+                </ul>
+              </md-card-content>
+              <md-card-actions layout="row" layout-align="start center" layout-padding style="display:none">
+                <div style="padding:0; display:none">
+                  <md-button class="md-raised md-primary">Continue Reading</md-button>
+                </div>
+              </md-card-actions>
+              <md-card-icon-actions layout layout-padding>
+                <span flex></span>
+                <div>
+                  <md-icon>question_answer</md-icon>
+                  <span>1.3k</span>
+                </div>
+                <div>
+                  <md-icon>favorite</md-icon>
+                  <span>2.4k</span>
+                </div>
+              </md-card-icon-actions>
+            </md-card>
+
+
         </section>
-        <section style="width:300px;" hide-xs>
-          <h3 class="md-headline label-type-1">Recent Activity</h3>
-          <nx-paper>
-            Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
-          </nx-paper>
+        <section style="width:340px;" hide-xs>
+          <h3 class="md-headline label-type-1" layout-margin>Recent Activity</h3>
+          <md-card>
+            <div style="height:100px;"></div>
+          </md-card>
         </section>
       </nx-container>
 
